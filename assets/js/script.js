518acea02f2c6e5da2556943c418c17550287f3c
jacknel <mjacknelson19@gmail.com>
2022-11-10 00:03:37 -0600

    restart with cloud recording, thank you tyler

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,43 @@
-var quizContent = document.querySelector('.quiz-content');
-var timer = document.querySelectorAll('.time-left');
-var qTitleElement = document.querySelector('#question-title');
-var startQuizElement = document.querySelector('#start-quiz');
-var startBtn = document.querySelector('#start-button');
-var answerChoices = document.querySelector('#answer-choices');
-var questionIndex = 0;
+const startBtn = document.querySelector('#start-btn');
+const timerDiv = document.querySelector('#timer');
+const questionsDiv = document.querySelector('#questions');
 
 
-var questions = [
+const questions = [
+        {
+        text: "The alert method is applied by which DOM element by default?",
+        choices: ["1. Header", "2. Window", "3. Body", "4. Footer"],
+        answer: "2. Window"
+    },
     {
-    questionTitle: "The alert method is applied by which DOM element by default?",
-    choices: ["1. Header", "2. Window", "3. Body", "4. Footer"],
-    answer: "2. Window"
-},
-{
-    questionTitle: "Which language is primarily used to added interactivity to a webpage?",
-    choices: ["1. HTML", "2. CSS", "3. Ruby on Rails", "4. JavaScript"],
-    answer: "4. JavaScript"
-}
+        text: "Which language is primarily used to added interactivity to a webpage?",
+        choices: ["1. HTML", "2. CSS", "3. Ruby on Rails", "4. JavaScript"],
+        answer: "4. JavaScript"
+    },
+    {
+        text: "question 3",
+        choices: ["1. A", "2. B", "3. C", "4. D"],
+        answer: "2. B"
+    },
+    {
+        text: "question 4",
+        choices: ["1. A", "2. B", "3. C", "4. D"],
+        answer: "2. B"
+    },
 ];
 
-function validateAnswer(){
-    console.log(this.value);
-    if (this.value === questions[questionIndex].answer){
-        questionIndex++;
-        console.log('here')
-        startQuiz();
-    } 
-};
+let questionIndex = 0;
+let time = 60;
+let quizTimer;
 
-function startQuiz(){
-    qTitleElement.textContent = questions[questionIndex].questionTitle;
-    answerChoices.textContent = '';
-    questions[questionIndex].choices.forEach(choice => {
-        var button = document.createElement('button');
-        button.textContent = choice;
-        button.setAttribute('value', choice);
-        button.onclick = validateAnswer;
-        answerChoices.appendChild(button);
+const startQuiz = () => {
+    quizTimer
+}
 
-    });
-};
+startBtn.addEventListener("click", function(event) {
+    
+});
 
-startBtn.addEventListener('click', function(){
-    console.log('here');
-    startQuizElement.setAttribute('class', 'hide');
-    quizContent.classList.remove('hide');
 
-    startQuiz();
-})
+
+// -------------------------------------------------------------------------------
